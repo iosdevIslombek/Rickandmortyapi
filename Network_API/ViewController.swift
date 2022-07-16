8514a54b149347bcfbfd0e11b0749dffc1a0bd68
iosdevIslombek <islomjonrazzoqov@gmail.com>
2022-07-16 09:50:09 +0500

    Initial Commit

---
+++ Network_API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Network_API
+//
+//  Created by Islomjon on 16/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
